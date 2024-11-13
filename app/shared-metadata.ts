87a30de9a912010f2b329bcaa8d13ed2c31b28eb
SamNanton <trikkiwun@gmail.com>
2024-11-13 17:05:17 -0500

    Update shared-metadata.ts
    add /preloads to precache data

--- app/shared-metadata.ts
+++ app/shared-metadata.ts
@@ -12,6 +12,20 @@ export const getMetadata = cache(async (brand: string) => {
       `${antelopeEndpoint}/chatbots/intro?origin=leadgen&shortcode=${brand}`
     )
 
+    const preloadRes = await fetcher(
+      `${antelopeEndpoint}/chatbots/preloads?shortcode=${brand}`
+    );
+    const { urls } = preloadRes.data || {};
+
+    if (urls && Array.isArray(urls)) {
+      Promise.allSettled(
+        urls.map((url) => fetcher(url))
+      ).then((backgroundResults) => {
+        console.log('Background fetch results:', backgroundResults);
+        // Optionally handle or log the results
+      });
+    }
+
     return res.data
   } catch (err) {
     console.log('error------>', err)
